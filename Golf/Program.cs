f920ef55d146abe5a4cad2847b926da7ee67a02f
Roger <rogerholmgren@gmail.com>
2016-11-04 16:24:24 +0100

    Friday evening commit

--- Golf/Program.cs
+++ Golf/Program.cs
@@ -8,9 +8,38 @@ namespace Golf
 {
     class Program
     {
+        private static readonly int CUP_POS = 900;
+        private static readonly int COURSE_LENGTH = 1000;
+        private static readonly int SWING_LIMIT = 10;
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine(PhysicsEngine.getAngleInRadians(90f));
+            var golfCourse = new GolfCourse(CUP_POS, COURSE_LENGTH, SWING_LIMIT);
+            var player = new Player(golfCourse);
+
+            Console.WriteLine("### Hello, Lets play some Golf! ###");
+            Console.WriteLine(golfCourse.ToString());
+            while (!player.IsDone())
+            {
+                Console.WriteLine(player.Info());
+
+                Console.Write("- Choose angle: ");
+                int angle;
+                int.TryParse(Console.ReadLine(), out angle);
+                Console.Write("- Choose velocity: ");
+                int velocity;
+                int.TryParse(Console.ReadLine(), out velocity);
+
+                player.HitTheBall(angle, velocity);
+
+                Console.WriteLine($"The ball flew {player.lastDistance()}");
+            }
+            Console.WriteLine($"You hit the cup!. Swings = {player.swings}");
+            foreach (var swing in player.swings)
+            {
+                Console.WriteLine(swing.);
+            }
         }
     }
 }
