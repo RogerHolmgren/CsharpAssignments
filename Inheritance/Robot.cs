030ddcb63358621dafbcc372bfce4cfde375d2c5
Roger <rogerholmgren@gmail.com>
2016-11-30 15:02:42 +0100

    Removed IMultiLocomotion

--- Inheritance/Robot.cs
+++ Inheritance/Robot.cs
@@ -6,9 +6,8 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    class Robot : MechanicalThing, IMultiLocomotion
+    class Robot : MechanicalThing, ILegLocomotion
     {
-        private string _state = "not set";
 
         public Robot(string Id, int CreationDate) : base(Id, CreationDate)
         {
@@ -21,36 +20,13 @@ namespace Inheritance
 
         public void Jump()
         {
-            if (_state == "legs")
-            {
-                Console.WriteLine($"{Id} jumped 10cm.");
-            }
-            else
-            {
-                Console.WriteLine($"{Id} cannot jump in current state({_state}).");
-            }
-
+            Console.WriteLine($"{Id} jumped 10cm.");
         }
 
         public void Move()
         {
-            if (_state == "legs")
-            {
-                Console.WriteLine($"{Id} walked 100m.");
-            }
-            else if (_state == "serpent")
-            {
-                Console.WriteLine($"{Id} slithered 40m.");
-            }
-            else
-            {
-                Console.WriteLine($"{Id} cannot move in current state({_state}).");
-            }
-        }
+            Console.WriteLine($"{Id} walked 100m.");
 
-        public void SetLocomotionState(string state)
-        {
-            this._state = state;
         }
     }
 }
